26e8065c76beb0297f17a64e523d168854949298
Kenji Hino <kenji@lotsofbytes.com>
2023-04-08 16:53:13 -0700

    wip

--- public/js/app.js
+++ public/js/app.js
@@ -2294,7 +2294,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "2177826700d96b30ed2c",
+  key: "767bad68770f71283fda",
   cluster: "us3",
   forceTLS: true,
   authEndpoint: '/laravel-chat/broadcasting/auth'
