d2bce07bf7ed2ca2f49ab9957d84eb6b8d54b6e7
Kenji Hino <kenji@lotsofbytes.com>
2023-05-12 11:40:33 -0700

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,32 +4,98 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-const { default: Echo } = require('laravel-echo');
-
-require('./bootstrap');
-
-window.Vue = require('vue').default;
+//const { default: Echo } = require('laravel-echo');
+//require('./bootstrap');
 
 /**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+window.axios = require('axios');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
-Vue.component('chat-form', require('./components/ChatForm.vue').default);
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+
+//window.Pusher = require('pusher-js');
+require('pusher-js');
+
+import Echo from 'laravel-echo';
+
+//window.Echo = new Echo({
+const laravelEcho = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+    authEndpoint: '/laravel-chat/broadcasting/auth'
+});
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const app = {
+    data() {
+        return {
+            messages: []
+        }
+    },
+
+    created() {
+        this.fetchMessages();
+
+        laravelEcho.private('chat')
+            .listen('MessageSent', (e) => {
+                this.messages.push({
+                    message: e.message.message,
+                    user: e.user
+                });
+            });
+    },
+
+    methods: {
+        fetchMessages() {
+            axios.get('/laravel-chat/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/laravel-chat/messages', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    },
+
+    components: {
+        'chat-messages' : require('./components/ChatMessages.vue').default,
+        'chat-form' : require('./components/ChatForm.vue').default,
+    }
+};
+
+import { createApp } from 'vue';
+
+
+createApp(app).mount('#app');
+
+/*
+
+window.Vue = require('vue').default;
+
+app.component('chat-messages', require('./components/ChatMessages.vue').default);
+app.component('chat-form', require('./components/ChatForm.vue').default);
 
 const app = new Vue({
     el: '#app',
@@ -64,4 +130,4 @@ const app = new Vue({
             });
         }
     }
-});
+}); */
