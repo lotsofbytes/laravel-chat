33ed8b4720aa7e4ab4b7aed94729215ba96dd3aa
Kenji Hino <kenji@lotsofbytes.com>
2023-10-18 16:00:02 -0700

    use ref instead of id

--- resources/js/components/ChatToast.vue
+++ resources/js/components/ChatToast.vue
@@ -1,28 +1,29 @@
 <script setup>
-import { onUpdated } from 'vue';
+import { onUpdated, ref } from 'vue';
 
 defineProps({
   toast: Object
 });
 
+let toastRef = ref(null);
+
 onUpdated(() => {
-  const toastLiveExample = document.getElementById('liveToast');
-  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastRef.value);
   toastBootstrap.show();
 });
 
 </script>
 
 <template>
-   <div class="toast-container position-fixed bottom-0 end-0 p-3">
-        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-          <div class="toast-header">
-            <strong class="me-auto">{{ toast.name }}さんへのお知らせ！</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-          </div>
-          <div class="toast-body">
-            {{ toast.message }}
-          </div>
-        </div>
+  <div class="toast-container position-fixed bottom-0 end-0 p-3">
+    <div ref=toastRef class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <strong class="me-auto">{{ toast.name }}さんへのお知らせ！</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">
+        {{ toast.message }}
+      </div>
     </div>
+  </div>
 </template>
